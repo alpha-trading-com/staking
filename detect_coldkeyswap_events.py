7ed4b687dde8277f32d5bb8f10b85a81feeb64dc
pycorn9729 <hayesdominique0729@gmail.com>
2025-12-08 15:21:07 +0100

    feat: add safe subnets

--- detect_coldkeyswap_events.py
+++ detect_coldkeyswap_events.py
@@ -192,8 +192,93 @@ class ColdkeySwapFetcher:
         # 4. No input validation issues for .get('subnet') because the dicts are always parsed the same way but type safety could be considered.
 
         SAFE_SUBNETS = {
-            40: 100,
-            101: 100,
+            7: 100, # pluton
+            10: 100, # pluton
+            11: 100, # pluton
+            12: 100, # pluton
+            13: 100, # pluton
+            15: 100, # pluton
+            16: 100, # pluton
+            17: 100, # pluton
+            18: 100, # pluton
+            19: 100, # pluton
+            20: 100, # pluton
+            21: 100, # pluton
+            22: 100, # pluton
+            24: 100, # pluton
+            25: 100, # pluton
+            26: 100, # pluton
+            27: 100, # pluton
+            28: 100, # fish
+            29: 100, # coldint
+            31: 100, # pluton
+            32: 100, # pluton
+            33: 100, # pluton
+            34: 100, # pluton
+            36: 100, # pluton
+            37: 100, # pluton
+            38: 100, # pluton
+            40: 100, # chunking
+            42: 100, # pluton
+            43: 100, # pluton
+            44: 100, # pluton
+            47: 100, # pluton       
+            52: 100, # pluton
+            53: 100, # pluton
+            54: 100, # pluton
+            55: 100, # pluton
+            57: 100, # pluton
+            58: 100, # pluton
+            60: 100, # pluton
+            61: 100, # pluton
+            65: 100, # pluton
+            69: 100, # pluton
+            70: 100, # pluton
+            72: 100, # pluton
+            73: 100, # pluton
+            74: 100, # pluton
+            76: 100, # pluton
+            77: 100, # pluton
+            78: 40, # pluton
+            79: 100, # pluton
+            80: 100, # pluton
+            82: 40, # pluton
+            83: 100, # pluton
+            84: 100, # pluton
+            87: 40, # checkerchain
+            88: 100, # pluton
+            89: 100, # pluton
+            90: 100, # pluton
+            91: 100, # pluton
+            93: 100, # pluton
+            94: 100, # pluton
+            95: 100, # pluton
+            96: 100, # pluton
+            97: 100, # pluton
+            98: 100, # pluton
+            99: 100, # pluton
+            101: 100, # pluton
+            102: 100, # pluton
+            103: 100, # pluton
+            104: 100, # pluton
+            105: 100, # pluton
+            106: 100, # pluton
+            107: 100, # pluton
+            108: 100, # pluton
+            109: 100, # pluton
+            110: 100, # pluton
+            111: 100, # pluton
+            112: 100, # pluton
+            113: 100, # pluton
+            114: 20, # pluton
+            115: 100, # pluton
+            116: 100, # pluton
+            117: 100, # pluton
+            118: 100, # pluton
+            119: 100, # pluton
+            122: 100, # pluton
+            126: 100, # pluton
+            128: 100, # pluton
         }
         # Collect all relevant subnets from swaps and changes
         stake_candidates = set()
@@ -204,6 +289,7 @@ class ColdkeySwapFetcher:
                 continue
             if subnet_id in SAFE_SUBNETS:
                 stake_candidates.add(subnet_id)
+
         for change in identity_changes:
             try:
                 subnet_id = int(change['subnet'])
